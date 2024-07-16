62a997f06724cf8fbbd8cd6c54232e46060f3eac
Dynatroz <dynatroz@gmail.com>
2024-07-16 20:59:51 +0530

    1

---
+++ mainforslider.js
@@ -0,0 +1,30 @@
+let swiper = new Swiper(".slider-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    effect: 'coverflow',
+    // effect: 'cards',
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    },
+});
